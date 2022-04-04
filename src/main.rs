e6a00042b7a7ffde269f6c90cd373084b57a71b9
Demir Yerli <mrquantumoff@protonmail.com>
2022-04-04 08:12:06 +0300

    Add args support to "launch_command" command

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ fn main() {
     std::io::stdin().read_line(&mut input).unwrap();
     let input = input.trim();
     if input == "1" {
-        launch_command("pfetch");
+        launch_command("pfetch", "-d");
     }
     if input == "2" {
         download_file("https://gist.githubusercontent.com/mrquantumoff/0b443e43759830f88075514dfdae8df4/raw/ef47c07edc54909e668ce9b7625ff3f73d097a64/.zshrc");
     }
 }
-fn launch_command(command: &str) {
+fn launch_command(command: &str, args: &str) {
     let output = Command::new(command)
+        .arg(args)
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
 fn download_file(url: &str) {
@@ -25,6 +26,6 @@ fn download_file(url: &str) {
         .arg(url)
         .arg("\"")
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
