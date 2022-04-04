4b07fcf0a20381406da778a35b41bf6ed2718e02
Demir Yerli <mrquantumoff@protonmail.com>
2022-04-04 10:47:54 +0300

    Start work on data_load, enhance download_file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
-fn main() {
+ fn main() {
     println!("Which function do you want to test?");
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
@@ -8,24 +8,36 @@ fn main() {
         launch_command("pfetch", "-d");
     }
     if input == "2" {
-        download_file("https://gist.githubusercontent.com/mrquantumoff/0b443e43759830f88075514dfdae8df4/raw/ef47c07edc54909e668ce9b7625ff3f73d097a64/.zshrc");
+        download_file("https://gist.githubusercontent.com/mrquantumoff/0b443e43759830f88075514dfdae8df4/raw/ef47c07edc54909e668ce9b7625ff3f73d097a64/.zshrc", "./.qzshrc");
     }
 }
-fn launch_command(command: &str, args: &str) {
+pub fn launch_command(command: &str, args: &str) {
     let output = Command::new(command)
         .arg(args)
         .output()
         .expect("Failed to execute process");
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
-fn download_file(url: &str) {
+pub fn download_file(url: &str, output_file: &str) {
     println!("Downloading {}", url);
-    let output = Command::new("wget")
+    let output = Command::new("/bin/wget")
         .arg("-q")
+        .arg("-O \'")
+        .arg(output_file)
+        .arg("\'")
         .arg("\"")
         .arg(url)
         .arg("\"")
         .output()
         .expect("Failed to execute process");
     println!("{}", String::from_utf8_lossy(&output.stdout));
+}
+pub fn data_load(Type: &str, file: &str) {
+    println!("Loading {}", file);
+    let output = Command::new("/bin/cat")
+        .arg(file)
+        .output()
+        .expect("Failed to execute process");
+    let mut linecount: i32 = 0;
+    //println!("{}", String::from_utf8_lossy(&output.stdout));
 }
