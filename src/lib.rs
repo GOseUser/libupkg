0a69e3b4cebb6e78135f865ad4878fd9d1917cd1
Demir Yerli <mrquantumoff@protonmail.com>
2022-04-05 11:34:24 +0300

    add more getters

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #[allow(unused_imports)]
-use std::{env, fs, io, process::*, string::*};
+use std::{env, fs, io, process::*, string::*, path::Path};
 #[allow(dead_code)]
 
 #[allow(non_upper_case_globals)]
@@ -58,8 +58,12 @@ pub struct versions {
     pub versions_names: Vec<String>,
     pub versions_versions: Vec<i32>,
 }
+
+
+
 #[allow(unused_mut)]
 pub fn get_versions() -> versions {
+    // Grabs only the version numbers from the packages.Ud file (installed versions)
     let contents = fs::read_to_string(packages_file).expect("Something went wrong reading the file");
     let mut pkgs_names: Vec<String> = Vec::new();
     let mut pkgs_versions: Vec<i32> = Vec::new();
@@ -83,29 +87,85 @@ pub fn get_versions() -> versions {
     };
 }
 #[allow(unused_mut)]
-
-pub fn get_app_names_and_urls(file: String) -> app_names_and_urls {
-    let contents = fs::read_to_string(file).expect("Something went wrong reading the file");
-    let mut app_names: Vec<String> = Vec::new();
-    let mut app_urls: Vec<String> = Vec::new();
-    let conts = &contents;
-    let conts_split = conts.split("\n");
-    for cont in conts_split {
-        if cont.contains("###COMMENT###") {
-            continue;
-        }
-        else {
-            let mut splitted = cont.split("|");
-            let mut name = splitted.next().unwrap();
-            let mut url = splitted.next().unwrap();
-            app_names.push(name.to_string());
-            app_urls.push(url.to_string());
+pub fn get_updated_data_urls() -> app_names_and_urls {
+    // Loads data from <repo>.Ud files for each repo
+    let mut apps_names: Vec<String> = Vec::new();
+    let mut apps_urls: Vec<String> = Vec::new();
+    let mut sources_names: Vec<String> = Vec::new();
+    for source in sources_names {
+        let srcdir: String = config_path.to_string() + &source;
+        let mut contents = fs::read_to_string(srcdir).expect("Something went wrong reading the file");
+        let mut conts = &contents;
+        let conts_split = conts.split("\n");
+        for cont in conts_split {
+            if cont.contains("###COMMENT###") {
+                continue;
+            }
+            else {
+                let type1 = "app_name_and_url, ";
+                if cont.contains(type1) {
+                    let namevalue = cont.replace(type1, "");
+                    let mut splitted = namevalue.split("|");
+                    let name = splitted.next().unwrap();
+                    let url = splitted.next().unwrap();
+                    apps_names.push(name.to_string());
+                    apps_urls.push(url.to_string());
+                }       
+            }
         }
     }
     return app_names_and_urls {
-        app_names: app_names,
-        app_urls: app_urls,
+        app_names: apps_names,
+        app_urls: apps_urls,
     };
+}
+#[allow(unused_mut)]
+pub fn update_app(package: String, version: i32){
+    // Rewrites the packages.Ud file with the new version number for the package that was specified
+    let mut contents = fs::read_to_string(packages_file).expect("Something went wrong reading the file");
+    let mut conts = &contents;
+    let conts_split = conts.split("\n");
+    let mut new_contents = String::new();
+    for cont in conts_split {
+        if cont.contains("###COMMENT###") {
+            new_contents.push_str(cont);
+            new_contents.push_str("\n");
+        }
+        else {
+            let mut splitted = cont.split("|");
+            let name = splitted.next().unwrap();
+            if name == package {
+                new_contents.push_str(&format!("{}|{}", name, version));
+                new_contents.push_str("\n");
+            }
+            else {
+                new_contents.push_str(cont);
+                new_contents.push_str("\n");
+            }
+        }
+    }
+    fs::write(packages_file, new_contents).expect("Something went wrong writing to the file");
+}
+
+#[allow(unused_mut)]
+pub fn add_app(package: String, version: i32) {
+    // Adds a new package to the packages.Ud file
+    let mut apps = get_versions();
+    let mut app_names = apps.versions_names.clone();
+    let mut app_versions = apps.versions_versions.clone();
+    app_names.push(package);
+    app_versions.push(version);
+    let mut new_contents = String::new();
+    for (i, name) in app_names.iter().enumerate() {
+        new_contents.push_str(name);
+        new_contents.push_str("|");
+        new_contents.push_str(&app_versions[i].to_string());
+        new_contents.push_str("\n");
+    }
+    
+    fs::write(packages_file, new_contents).expect("Unable to write to file");
+}
+
 #[allow (dead_code)]
 pub fn get_sources() -> repos {
     let contents = fs::read_to_string(sources_file).expect("Something went wrong reading the file");
@@ -130,7 +190,40 @@ pub fn get_sources() -> repos {
         repos_urls: repos_urls,
     };
 }
-
+#[allow(dead_code)]
+pub fn get_latestversions() -> versions {
+    // Grabs the latest versions from the <repo>.Ud files
+    let repositories = get_sources();
+    let mut repos_names = repositories.repos_names.clone();
+    let mut app_names: Vec<String> = Vec::new();
+    let mut app_versions: Vec<i32> = Vec::new();
+    for source in repos_names {
+        let srcdir: String = config_path.to_string() + &source;
+        let mut contents = fs::read_to_string(srcdir).expect("Something went wrong reading the file, most probably file not found");
+        let mut conts = &contents;
+        let conts_split = conts.split("\n");
+        for cont in conts_split {
+            if cont.contains("###COMMENT###") {
+                continue;
+            }
+            else {
+                let type1 = "app_name_and_version, ";
+                if cont.contains(type1) {
+                    let namevalue = cont.replace(type1, "");
+                    let mut splitted = namevalue.split("|");
+                    let name = splitted.next().unwrap();
+                    let ver = splitted.next().unwrap();
+                    app_names.push(name.to_string());
+                    app_versions.push(ver.parse::<i32>().unwrap());
+                }       
+            }
+        }
+    }
+    return versions {
+        versions_names: app_names,
+        versions_versions: app_versions,
+    };
+}
 
 
 /*
@@ -160,4 +253,4 @@ pub fn data_get(Type: &str, file: &str) {
         
     }
 }*/
-}
+
